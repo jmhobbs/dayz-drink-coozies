b5b076d1572d15761777152cbfcb97de1c3ed856
John Hobbs <john@velvetcache.org>
2026-04-24 17:01:40 -0500

    Initial model and mod stub

---
+++ Drink_Coozies/config.cpp
@@ -0,0 +1,25 @@
+class CfgPatches
+{
+  class Drink_Coozies
+  {
+    units[] = {};
+    weapons[] = {};
+    requiredVersion = 0.1;
+    requiredAddons[] = {
+      "DZ_Data",
+      "DZ_Scripts",
+      "DZ_Gear_Drinks",
+    };
+  };
+};
+class CfgMods
+{
+  class Drink_Coozies
+  {
+    type = "mod";
+    author = "Hopps";
+    name = "Drink_Coozies"; 
+    dir = "Drink_Coozies";
+    dependencies[] = {};
+  };
+};
